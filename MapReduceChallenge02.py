6ee2f92c81ad1e90d81974b953db7e60ac19a2a8
Syed Rafay <rafay_syed1@yahoo.com>
2018-08-26 14:45:45 +0500

    Added script 'MapReduceChallenge02.py' for MapReduce

---
+++ MapReduceChallenge02.py
@@ -0,0 +1,26 @@
+# MapReduceChallenge02.py
+from mrjob.job import MRJob
+from mrjob.step import MRStep
+
+class RatingsBreakdown(MRJob):
+
+	def steps(self):
+		return [
+			MRStep( mapper=self.mapper_get_ratings,
+				reducer=self.reducer_count_ratings),
+			MRStep( reducer=self.reducer_sorted_output)
+		]
+    
+	def mapper_get_ratings(self, _, line):
+		(userID, movieID, rating, timestamp) = line.split('\t')
+		yield movieID, 1
+    
+	def reducer_count_ratings(self, key, values):
+		yield str(sum(values)).zfill(5), key
+	
+	def reducer_sorted_output(self, count, movies):
+		for movie in movies:
+			yield movie, count
+    
+if __name__ == '__main__':
+	RatingBreakdown.run()
